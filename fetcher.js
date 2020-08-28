34ae19d91d3cc3e5d3a3ed0a86542bc0cac66b4c
Blakexx <blake@platypus.land>
2020-08-28 12:09:20 -0500

    Comments

--- fetcher.js
+++ fetcher.js
@@ -4,15 +4,19 @@ var extra = require("fs-extra");
 
 let options = require("./options.json");
 
+//Set up the requested test files
 let toFetch = [];
 options["tests"].forEach(s=>{
 	toFetch.push(s+".cc");
 	toFetch.push(s+".ok");
 });
 
+//Retrieve all requested tests concurrently
 Promise.all(toFetch.map(s=>fetch(options["testsUrl"]+"/"+s))).then(responses=>{
+	//Map response to text
 	return Promise.all(responses.map(response => response.text()));
 }).then(data=>{
+	//For each fetched file, write it to the project directory
 	let projectPath = options["projectPath"];
 	for(let i = 0; i<toFetch.length;i++){
 		let filePath = projectPath+"/"+toFetch[i];
